d950afbad3f50fa0a7606ebdca1a91e0db18fff2
Armin Fuerst <armin@fuerst.priv.at>
2021-03-07 02:35:44 +0100

    moved rotate_serial

--- apps/lib/apps_extracted.c
+++ apps/lib/apps_extracted.c
@@ -900,50 +900,50 @@ int save_serial(const char *serialfile, const char *suffix, const BIGNUM *serial
     return ret;
 }
 
-//int rotate_serial(const char *serialfile, const char *new_suffix,
-//                  const char *old_suffix)
-//{
-//    char buf[2][BSIZE];
-//    int i, j;
-//
-//    i = strlen(serialfile) + strlen(old_suffix);
-//    j = strlen(serialfile) + strlen(new_suffix);
-//    if (i > j)
-//        j = i;
-//    if (j + 1 >= BSIZE) {
-//        BIO_printf(bio_err, "File name too long\n");
-//        goto err;
-//    }
-//#ifndef OPENSSL_SYS_VMS
-//    j = BIO_snprintf(buf[0], sizeof(buf[0]), "%s.%s", serialfile, new_suffix);
-//    j = BIO_snprintf(buf[1], sizeof(buf[1]), "%s.%s", serialfile, old_suffix);
-//#else
-//    j = BIO_snprintf(buf[0], sizeof(buf[0]), "%s-%s", serialfile, new_suffix);
-//    j = BIO_snprintf(buf[1], sizeof(buf[1]), "%s-%s", serialfile, old_suffix);
-//#endif
-//    if (app_rename(serialfile, buf[1]) < 0 && errno != ENOENT
-//#ifdef ENOTDIR
-//        && errno != ENOTDIR
-//#endif
-//        ) {
-//        BIO_printf(bio_err,
-//                   "Unable to rename %s to %s\n", serialfile, buf[1]);
-//        perror("reason");
-//        goto err;
-//    }
-//    if (app_rename(buf[0], serialfile) < 0) {
-//        BIO_printf(bio_err,
-//                   "Unable to rename %s to %s\n", buf[0], serialfile);
-//        perror("reason");
-//        app_rename(buf[1], serialfile);
-//        goto err;
-//    }
-//    return 1;
-// err:
-//    ERR_print_errors(bio_err);
-//    return 0;
-//}
-//
+int rotate_serial(const char *serialfile, const char *new_suffix,
+                  const char *old_suffix)
+{
+    char buf[2][BSIZE];
+    int i, j;
+
+    i = strlen(serialfile) + strlen(old_suffix);
+    j = strlen(serialfile) + strlen(new_suffix);
+    if (i > j)
+        j = i;
+    if (j + 1 >= BSIZE) {
+        BIO_printf(bio_err, "File name too long\n");
+        goto err;
+    }
+#ifndef OPENSSL_SYS_VMS
+    j = BIO_snprintf(buf[0], sizeof(buf[0]), "%s.%s", serialfile, new_suffix);
+    j = BIO_snprintf(buf[1], sizeof(buf[1]), "%s.%s", serialfile, old_suffix);
+#else
+    j = BIO_snprintf(buf[0], sizeof(buf[0]), "%s-%s", serialfile, new_suffix);
+    j = BIO_snprintf(buf[1], sizeof(buf[1]), "%s-%s", serialfile, old_suffix);
+#endif
+    if (app_rename(serialfile, buf[1]) < 0 && errno != ENOENT
+#ifdef ENOTDIR
+        && errno != ENOTDIR
+#endif
+        ) {
+        BIO_printf(bio_err,
+                   "Unable to rename %s to %s\n", serialfile, buf[1]);
+        perror("reason");
+        goto err;
+    }
+    if (app_rename(buf[0], serialfile) < 0) {
+        BIO_printf(bio_err,
+                   "Unable to rename %s to %s\n", buf[0], serialfile);
+        perror("reason");
+        app_rename(buf[1], serialfile);
+        goto err;
+    }
+    return 1;
+ err:
+    ERR_print_errors(bio_err);
+    return 0;
+}
+
 //int rand_serial(BIGNUM *b, ASN1_INTEGER *ai)
 //{
 //    BIGNUM *btmp;
