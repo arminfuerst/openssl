039ef12688de7209f564901178271fe6e368fee1
Armin Fuerst <armin@fuerst.priv.at>
2021-03-07 11:02:08 +0100

    moved rotate_index

--- apps/lib/apps_extracted.c
+++ apps/lib/apps_extracted.c
@@ -1107,73 +1107,73 @@ int save_index(const char *dbfile, const char *suffix, CA_DB *db)
     return 0;
 }
 
-//int rotate_index(const char *dbfile, const char *new_suffix,
-//                 const char *old_suffix)
-//{
-//    char buf[5][BSIZE];
-//    int i, j;
-//
-//    i = strlen(dbfile) + strlen(old_suffix);
-//    j = strlen(dbfile) + strlen(new_suffix);
-//    if (i > j)
-//        j = i;
-//    if (j + 6 >= BSIZE) {
-//        BIO_printf(bio_err, "File name too long\n");
-//        goto err;
-//    }
-//#ifndef OPENSSL_SYS_VMS
-//    j = BIO_snprintf(buf[4], sizeof(buf[4]), "%s.attr", dbfile);
-//    j = BIO_snprintf(buf[3], sizeof(buf[3]), "%s.attr.%s", dbfile, old_suffix);
-//    j = BIO_snprintf(buf[2], sizeof(buf[2]), "%s.attr.%s", dbfile, new_suffix);
-//    j = BIO_snprintf(buf[1], sizeof(buf[1]), "%s.%s", dbfile, old_suffix);
-//    j = BIO_snprintf(buf[0], sizeof(buf[0]), "%s.%s", dbfile, new_suffix);
-//#else
-//    j = BIO_snprintf(buf[4], sizeof(buf[4]), "%s-attr", dbfile);
-//    j = BIO_snprintf(buf[3], sizeof(buf[3]), "%s-attr-%s", dbfile, old_suffix);
-//    j = BIO_snprintf(buf[2], sizeof(buf[2]), "%s-attr-%s", dbfile, new_suffix);
-//    j = BIO_snprintf(buf[1], sizeof(buf[1]), "%s-%s", dbfile, old_suffix);
-//    j = BIO_snprintf(buf[0], sizeof(buf[0]), "%s-%s", dbfile, new_suffix);
-//#endif
-//    if (app_rename(dbfile, buf[1]) < 0 && errno != ENOENT
-//#ifdef ENOTDIR
-//        && errno != ENOTDIR
-//#endif
-//        ) {
-//        BIO_printf(bio_err, "Unable to rename %s to %s\n", dbfile, buf[1]);
-//        perror("reason");
-//        goto err;
-//    }
-//    if (app_rename(buf[0], dbfile) < 0) {
-//        BIO_printf(bio_err, "Unable to rename %s to %s\n", buf[0], dbfile);
-//        perror("reason");
-//        app_rename(buf[1], dbfile);
-//        goto err;
-//    }
-//    if (app_rename(buf[4], buf[3]) < 0 && errno != ENOENT
-//#ifdef ENOTDIR
-//        && errno != ENOTDIR
-//#endif
-//        ) {
-//        BIO_printf(bio_err, "Unable to rename %s to %s\n", buf[4], buf[3]);
-//        perror("reason");
-//        app_rename(dbfile, buf[0]);
-//        app_rename(buf[1], dbfile);
-//        goto err;
-//    }
-//    if (app_rename(buf[2], buf[4]) < 0) {
-//        BIO_printf(bio_err, "Unable to rename %s to %s\n", buf[2], buf[4]);
-//        perror("reason");
-//        app_rename(buf[3], buf[4]);
-//        app_rename(dbfile, buf[0]);
-//        app_rename(buf[1], dbfile);
-//        goto err;
-//    }
-//    return 1;
-// err:
-//    ERR_print_errors(bio_err);
-//    return 0;
-//}
-//
+int rotate_index(const char *dbfile, const char *new_suffix,
+                 const char *old_suffix)
+{
+    char buf[5][BSIZE];
+    int i, j;
+
+    i = strlen(dbfile) + strlen(old_suffix);
+    j = strlen(dbfile) + strlen(new_suffix);
+    if (i > j)
+        j = i;
+    if (j + 6 >= BSIZE) {
+        BIO_printf(bio_err, "File name too long\n");
+        goto err;
+    }
+#ifndef OPENSSL_SYS_VMS
+    j = BIO_snprintf(buf[4], sizeof(buf[4]), "%s.attr", dbfile);
+    j = BIO_snprintf(buf[3], sizeof(buf[3]), "%s.attr.%s", dbfile, old_suffix);
+    j = BIO_snprintf(buf[2], sizeof(buf[2]), "%s.attr.%s", dbfile, new_suffix);
+    j = BIO_snprintf(buf[1], sizeof(buf[1]), "%s.%s", dbfile, old_suffix);
+    j = BIO_snprintf(buf[0], sizeof(buf[0]), "%s.%s", dbfile, new_suffix);
+#else
+    j = BIO_snprintf(buf[4], sizeof(buf[4]), "%s-attr", dbfile);
+    j = BIO_snprintf(buf[3], sizeof(buf[3]), "%s-attr-%s", dbfile, old_suffix);
+    j = BIO_snprintf(buf[2], sizeof(buf[2]), "%s-attr-%s", dbfile, new_suffix);
+    j = BIO_snprintf(buf[1], sizeof(buf[1]), "%s-%s", dbfile, old_suffix);
+    j = BIO_snprintf(buf[0], sizeof(buf[0]), "%s-%s", dbfile, new_suffix);
+#endif
+    if (app_rename(dbfile, buf[1]) < 0 && errno != ENOENT
+#ifdef ENOTDIR
+        && errno != ENOTDIR
+#endif
+        ) {
+        BIO_printf(bio_err, "Unable to rename %s to %s\n", dbfile, buf[1]);
+        perror("reason");
+        goto err;
+    }
+    if (app_rename(buf[0], dbfile) < 0) {
+        BIO_printf(bio_err, "Unable to rename %s to %s\n", buf[0], dbfile);
+        perror("reason");
+        app_rename(buf[1], dbfile);
+        goto err;
+    }
+    if (app_rename(buf[4], buf[3]) < 0 && errno != ENOENT
+#ifdef ENOTDIR
+        && errno != ENOTDIR
+#endif
+        ) {
+        BIO_printf(bio_err, "Unable to rename %s to %s\n", buf[4], buf[3]);
+        perror("reason");
+        app_rename(dbfile, buf[0]);
+        app_rename(buf[1], dbfile);
+        goto err;
+    }
+    if (app_rename(buf[2], buf[4]) < 0) {
+        BIO_printf(bio_err, "Unable to rename %s to %s\n", buf[2], buf[4]);
+        perror("reason");
+        app_rename(buf[3], buf[4]);
+        app_rename(dbfile, buf[0]);
+        app_rename(buf[1], dbfile);
+        goto err;
+    }
+    return 1;
+ err:
+    ERR_print_errors(bio_err);
+    return 0;
+}
+
 //void free_index(CA_DB *db)
 //{
 //    if (db) {
