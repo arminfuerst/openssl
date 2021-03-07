92b613883ef5aff4f3705373dc322c9f3a380f98
Armin Fuerst <armin@fuerst.priv.at>
2021-03-07 11:57:21 +0100

    moved parse_name

--- apps/lib/apps.c
+++ apps/lib/apps.c
@@ -1595,111 +1595,111 @@ int index_name_cmp(const OPENSSL_CSTRING *a, const OPENSSL_CSTRING *b)
  * where + can be used instead of / to form multi-valued RDNs if canmulti
  * and characters may be escaped by \
  */
-X509_NAME *parse_name(const char *cp, int chtype, int canmulti,
-                      const char *desc)
-{
-    int nextismulti = 0;
-    char *work;
-    X509_NAME *n;
-
-    if (*cp++ != '/') {
-        BIO_printf(bio_err,
-                   "%s: %s name is expected to be in the format "
-                   "/type0=value0/type1=value1/type2=... where characters may "
-                   "be escaped by \\. This name is not in that format: '%s'\n",
-                   opt_getprog(), desc, --cp);
-        return NULL;
-    }
-
-    n = X509_NAME_new();
-    if (n == NULL) {
-        BIO_printf(bio_err, "%s: Out of memory\n", opt_getprog());
-        return NULL;
-    }
-    work = OPENSSL_strdup(cp);
-    if (work == NULL) {
-        BIO_printf(bio_err, "%s: Error copying %s name input\n",
-                   opt_getprog(), desc);
-        goto err;
-    }
-
-    while (*cp != '\0') {
-        char *bp = work;
-        char *typestr = bp;
-        unsigned char *valstr;
-        int nid;
-        int ismulti = nextismulti;
-        nextismulti = 0;
-
-        /* Collect the type */
-        while (*cp != '\0' && *cp != '=')
-            *bp++ = *cp++;
-        *bp++ = '\0';
-        if (*cp == '\0') {
-            BIO_printf(bio_err,
-                       "%s: Missing '=' after RDN type string '%s' in %s name string\n",
-                       opt_getprog(), typestr, desc);
-            goto err;
-        }
-        ++cp;
-
-        /* Collect the value. */
-        valstr = (unsigned char *)bp;
-        for (; *cp != '\0' && *cp != '/'; *bp++ = *cp++) {
-            /* unescaped '+' symbol string signals further member of multiRDN */
-            if (canmulti && *cp == '+') {
-                nextismulti = 1;
-                break;
-            }
-            if (*cp == '\\' && *++cp == '\0') {
-                BIO_printf(bio_err,
-                           "%s: Escape character at end of %s name string\n",
-                           opt_getprog(), desc);
-                goto err;
-            }
-        }
-        *bp++ = '\0';
-
-        /* If not at EOS (must be + or /), move forward. */
-        if (*cp != '\0')
-            ++cp;
-
-        /* Parse */
-        nid = OBJ_txt2nid(typestr);
-        if (nid == NID_undef) {
-            BIO_printf(bio_err,
-                       "%s: Skipping unknown %s name attribute \"%s\"\n",
-                       opt_getprog(), desc, typestr);
-            if (ismulti)
-                BIO_printf(bio_err,
-                           "Hint: a '+' in a value string needs be escaped using '\\' else a new member of a multi-valued RDN is expected\n");
-            continue;
-        }
-        if (*valstr == '\0') {
-            BIO_printf(bio_err,
-                       "%s: No value provided for %s name attribute \"%s\", skipped\n",
-                       opt_getprog(), desc, typestr);
-            continue;
-        }
-        if (!X509_NAME_add_entry_by_NID(n, nid, chtype,
-                                        valstr, strlen((char *)valstr),
-                                        -1, ismulti ? -1 : 0)) {
-            ERR_print_errors(bio_err);
-            BIO_printf(bio_err,
-                       "%s: Error adding %s name attribute \"/%s=%s\"\n",
-                       opt_getprog(), desc, typestr ,valstr);
-            goto err;
-        }
-    }
-
-    OPENSSL_free(work);
-    return n;
-
- err:
-    X509_NAME_free(n);
-    OPENSSL_free(work);
-    return NULL;
-}
+//X509_NAME *parse_name(const char *cp, int chtype, int canmulti,
+//                      const char *desc)
+//{
+//    int nextismulti = 0;
+//    char *work;
+//    X509_NAME *n;
+//
+//    if (*cp++ != '/') {
+//        BIO_printf(bio_err,
+//                   "%s: %s name is expected to be in the format "
+//                   "/type0=value0/type1=value1/type2=... where characters may "
+//                   "be escaped by \\. This name is not in that format: '%s'\n",
+//                   opt_getprog(), desc, --cp);
+//        return NULL;
+//    }
+//
+//    n = X509_NAME_new();
+//    if (n == NULL) {
+//        BIO_printf(bio_err, "%s: Out of memory\n", opt_getprog());
+//        return NULL;
+//    }
+//    work = OPENSSL_strdup(cp);
+//    if (work == NULL) {
+//        BIO_printf(bio_err, "%s: Error copying %s name input\n",
+//                   opt_getprog(), desc);
+//        goto err;
+//    }
+//
+//    while (*cp != '\0') {
+//        char *bp = work;
+//        char *typestr = bp;
+//        unsigned char *valstr;
+//        int nid;
+//        int ismulti = nextismulti;
+//        nextismulti = 0;
+//
+//        /* Collect the type */
+//        while (*cp != '\0' && *cp != '=')
+//            *bp++ = *cp++;
+//        *bp++ = '\0';
+//        if (*cp == '\0') {
+//            BIO_printf(bio_err,
+//                       "%s: Missing '=' after RDN type string '%s' in %s name string\n",
+//                       opt_getprog(), typestr, desc);
+//            goto err;
+//        }
+//        ++cp;
+//
+//        /* Collect the value. */
+//        valstr = (unsigned char *)bp;
+//        for (; *cp != '\0' && *cp != '/'; *bp++ = *cp++) {
+//            /* unescaped '+' symbol string signals further member of multiRDN */
+//            if (canmulti && *cp == '+') {
+//                nextismulti = 1;
+//                break;
+//            }
+//            if (*cp == '\\' && *++cp == '\0') {
+//                BIO_printf(bio_err,
+//                           "%s: Escape character at end of %s name string\n",
+//                           opt_getprog(), desc);
+//                goto err;
+//            }
+//        }
+//        *bp++ = '\0';
+//
+//        /* If not at EOS (must be + or /), move forward. */
+//        if (*cp != '\0')
+//            ++cp;
+//
+//        /* Parse */
+//        nid = OBJ_txt2nid(typestr);
+//        if (nid == NID_undef) {
+//            BIO_printf(bio_err,
+//                       "%s: Skipping unknown %s name attribute \"%s\"\n",
+//                       opt_getprog(), desc, typestr);
+//            if (ismulti)
+//                BIO_printf(bio_err,
+//                           "Hint: a '+' in a value string needs be escaped using '\\' else a new member of a multi-valued RDN is expected\n");
+//            continue;
+//        }
+//        if (*valstr == '\0') {
+//            BIO_printf(bio_err,
+//                       "%s: No value provided for %s name attribute \"%s\", skipped\n",
+//                       opt_getprog(), desc, typestr);
+//            continue;
+//        }
+//        if (!X509_NAME_add_entry_by_NID(n, nid, chtype,
+//                                        valstr, strlen((char *)valstr),
+//                                        -1, ismulti ? -1 : 0)) {
+//            ERR_print_errors(bio_err);
+//            BIO_printf(bio_err,
+//                       "%s: Error adding %s name attribute \"/%s=%s\"\n",
+//                       opt_getprog(), desc, typestr ,valstr);
+//            goto err;
+//        }
+//    }
+//
+//    OPENSSL_free(work);
+//    return n;
+//
+// err:
+//    X509_NAME_free(n);
+//    OPENSSL_free(work);
+//    return NULL;
+//}
 
 /*
  * Read whole contents of a BIO into an allocated memory buffer and return
